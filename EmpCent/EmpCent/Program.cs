40e26a7c65805273045fa1a7054584c178130d9f
Vitor D <vfrd00@ua.pt>
2021-06-16 19:27:50 +0100

    Init

---
+++ EmpCent/EmpCent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpCent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
